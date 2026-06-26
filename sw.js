0f3db2dbe167b8777b551e33ad21335e92276bbe
K-blip7 <decimal314@proton.me>
2026-06-26 23:13:22 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "calorie-quest-v85";
+const CACHE_NAME = "calorie-quest-v86";
 self.addEventListener("install", e => { self.skipWaiting(); });
 self.addEventListener("activate", e => {
   e.waitUntil(
